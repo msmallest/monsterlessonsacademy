3a126a68f55bbdaf42966dbb7cef8e8aa84ba472
Michael Small <michael.small@relationshipone.com>
2024-01-31 15:09:03 -0600

    Update everything else to v17

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { isDevMode, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsModule } from './posts/posts.module';
@@ -15,12 +13,6 @@ import { PostsComponent } from './posts/components/posts/posts.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    StoreModule.forRoot({}),
-    StoreDevtoolsModule.instrument({
-      maxAge: 25,
-      logOnly: !isDevMode(),
-      autoPause: true,
-    }),
     PostsModule,
     RouterModule
   ],
